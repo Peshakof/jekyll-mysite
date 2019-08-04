db074f74ebb4bd4d02440826835ea3508e812f27
Peshakof <nfs2000@abv.bg>
2019-08-04 12:52:22 +0300

    added smooth scrolling button

--- scrypts/site.js
+++ scrypts/site.js
@@ -1,3 +1,20 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementsByTagName('body')[0].classList.add('loaded');
+$(document).ready( function($){
+	
+	let offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
+		animationDuration = 1000, 
+		$backToTop = $('#toTop');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $backToTop.addClass('cd-is-visible') : $backToTop.removeClass('cd-is-visible');
+	});
+
+	//smooth scroll to top
+	$backToTop.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, animationDuration
+		);
+	});
 });
